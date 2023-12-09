a327a2bb9269d9b507fe591b0f99927a5dea2e6e
Ivan Veloz <devel@ivanveloz.com>
2023-12-09 16:36:51 -0500

    Added install assignment3 app and testscript deps

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 88baaff77f6a71b7a58361c5ebfd53ccc78d602f
+AESD_ASSIGNMENTS_VERSION = 3f162edef6fc645ff4f3b0dac9c892cf8b8fe45e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,6 +23,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/{finder*.sh,writer} $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
