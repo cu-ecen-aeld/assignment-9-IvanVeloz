db03073d54709e0f28e4dab88ebd47d9df3aedb2
Ivan Veloz <devel@ivanveloz.com>
2024-04-18 22:19:01 +0000

    Modified ldd.mk to build within the kernel build system

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -13,24 +13,26 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-IvanVeloz.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
+LDD_MODULE_SUBDIRS = misc-modules/
+LDD_MODULE_SUBDIRS += scull/
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED) KERNELDIR=$(LINUX_DIR)
+
 LDD_CONFIGURE_OPTS += KERNELDIR=$(LINUX_DIR)
 LDD_CONFIGURE_OPTS += ARCH=$(KERNEL_ARCH)
 LDD_CONFIGURE_OPTS += CROSS_COMPILE=$(TARGET_CROSS)
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) $(LDD_CONFIGURE_OPTS) -C $(@D)/misc-modules
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) $(LDD_CONFIGURE_OPTS) -C $(@D)/scull
-endef
-
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-#define LDD_INSTALL_TARGET_CMDS
-#	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-#	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-#	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin
-#	$(INSTALL) -m 0755 $(@D)/finder-app/{finder*.sh,writer} $(TARGET_DIR)/usr/bin
-#	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-#	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+# Uncomment to build outside the kernel build system
+#define LDD_BUILD_CMDS
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) $(LDD_CONFIGURE_OPTS) -C $(@D)/misc-modules modules
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) $(LDD_CONFIGURE_OPTS) -C $(@D)/scull modules
 #endef
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
